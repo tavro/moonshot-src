7034a98aa4f6cbdd20f9d4bacd807afae2d45ca4
Isak Horvath <isakh@opera.com>
2026-02-13 12:45:52 +0100

    ms: remove all files
    this is preparation for restructuring. i do not want to remove the files through the user interface, as that would be separated into multiple commits.
    
    i'm uploading the files through the interface, since i do not want to commit it using this git account (i'm lazy).

--- kernel.c
+++
@@ -1,6 +0,0 @@
-#include "uart.h"
-
-void kernel_main(void) {
-        uart_puts("Hello from knekt\n");
-        while(1);
-}
--- uart.h
+++
@@ -1,9 +0,0 @@
-#ifndef UART_H
-#define UART_H
-
-#define UART0 0x10000000UL
-
-void uart_putchar(char c);
-void uart_puts(const char *s);
-
-#endif
